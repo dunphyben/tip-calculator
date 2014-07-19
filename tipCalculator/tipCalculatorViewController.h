db75b244121fa1a12989f9bafdb5e2c14f81c0ac
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-07-19 10:34:43 -0700

    Initialize project. Add view controller

---
+++ tipCalculator/tipCalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  tipCalculatorViewController.h
+//  tipCalculator
+//
+//  Created by Benjamin Dunphy on 7/19/14.
+//  Copyright (c) 2014 Benjamin Dunphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tipCalculatorViewController : UIViewController
+
+@end
---
+++ tipCalculator/tipCalculatorViewController.m
@@ -0,0 +1,38 @@
+//
+//  tipCalculatorViewController.m
+//  tipCalculator
+//
+//  Created by Benjamin Dunphy on 7/19/14.
+//  Copyright (c) 2014 Benjamin Dunphy. All rights reserved.
+//
+
+#import "tipCalculatorViewController.h"
+
+@interface tipCalculatorViewController ()
+
+@end
+
+@implementation tipCalculatorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
