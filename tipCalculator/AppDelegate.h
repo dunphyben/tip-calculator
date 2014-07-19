77cffd8b88075fe36d32c1e15eda98f9d038d502
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-07-19 10:25:18 -0700

    Initial Commit

---
+++ tipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by Benjamin Dunphy on 7/19/14.
+//  Copyright (c) 2014 Benjamin Dunphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
